fe24ba6789624fb3b2942c22677c2ce50552577f
khoi123 <khoi123@KhoiNTSE63078.local>
2017-04-11 22:05:57 +0700

    make it better

--- server/server.js
+++ server/server.js
@@ -72,7 +72,7 @@ app.delete('/todos/:id', (req, res) => {
   if (!ObjectID.isValid(id)) {
     return res.status(404);
   }
-  if (_.isBoolean(body.completed) && body.completed) {
+  if (_.isBoolean(body.completed) && body.completed = true) {
     body.completedAt = new Date().getTime();
   } else {
     body.completed = false;
